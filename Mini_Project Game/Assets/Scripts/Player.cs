f3dfcc2dfaca9dc2c701b955125c0b09aa1920de
smhaoula <56696627+smhaoula@users.noreply.github.com>
2021-05-01 21:59:50 +0200

    Door and light Animation

--- Mini_Project Game/Assets/Scripts/Player.cs
+++ Mini_Project Game/Assets/Scripts/Player.cs
@@ -2,26 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    float speed = 3f; 
-    Vector3 bewegung = Vector3.zero;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow)){
-            bewegung = Vector3.forward;
-        }
-        if (Input.GetKey(KeyCode.DownArrow)){
-            bewegung = Vector3.back;
-        }
-        if (Input.GetKey(KeyCode.RightArrow)){
-            bewegung = Vector3.right;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow)){
-            bewegung = Vector3.left;
-        }
-        transform.Translate(bewegung * speed * Time.deltaTime);
+        
     }
 }

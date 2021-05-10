f3f321e9a59e3a4d0573d4961beec8ddc867d7f7
smhaoula <56696627+smhaoula@users.noreply.github.com>
2021-05-10 15:34:45 +0200

    Animat fixed

--- Mini_Project Game/Assets/Scripts/doorlevel2.cs
+++ Mini_Project Game/Assets/Scripts/doorlevel2.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlueDoor : MonoBehaviour
+public class doorlevel2 : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider other)
     {
-        
-        if (other.tag == "Player") {
-        
-            if(other.GetComponent<Movement>().HasBlueKey())
+
+        if (other.tag == "Player")
+        {
+
+
+            if (other.GetComponent<Movement>().HasYellowKey())
             {
-                
                 GetComponent<Animator>().enabled = true;
             }
-            
         }
     }
+
+
 }
